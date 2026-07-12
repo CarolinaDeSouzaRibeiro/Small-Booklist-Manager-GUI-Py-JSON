29aceef0323acf5dd4f827f146a6af52e2664781
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2026-07-12 19:01:37 -0300

    Update json_load_save.py

--- persistence/json_load_save.py
+++ persistence/json_load_save.py
@@ -6,9 +6,10 @@ import json
 from pathlib import Path
 from models.livro import Livro
 from datetime import date
+# import datetime
 
 # funções manipulação geral do json
-def salvar_json_livros(livrosObjetos:list, nome_json:str='livros.json'):
+def salvar_json_livros(livrosObjetos:dict, nome_json:str='livros.json'):
     '''Atualiza o JSON com os objetos livros
     no dict de execução da biblioteca.'''
 
@@ -20,8 +21,7 @@ def salvar_json_livros(livrosObjetos:list, nome_json:str='livros.json'):
              }, f) if livrosObjetos else json.dump({}, f) # Se não houverem livros na lista, coloca um json vazio
         #TODO: forma mais otimizada de salvar json, sem reescrever sempre do zero
 
-
-def carregar_livros_do_json(livrosObjetos=[], nome_json:str='livros.json'):
+def carregar_livros_do_json(livrosObjetos={}, nome_json:str='livros.json'):
     '''Carrega os livros no JSON para o dicionario de livros de execução'''
     #caso livros.json não exista
     if not Path(nome_json).is_file():
@@ -38,5 +38,5 @@ def carregar_livros_do_json(livrosObjetos=[], nome_json:str='livros.json'):
             return livrosObjetos
     except:
         print(f'\nErro ao carregar {nome_json}.\nCriando backup...\n')
-        Path(nome_json).rename(f'BACKUP_livros_{date.today()}.json.bak')
+        Path(nome_json).rename(f'BACKUP_livros_{date.today().strftime("%Y-%m-%d_%H-%M-%S")}.json.bak')
         return salvar_json_livros(livrosObjetos, nome_json) #tenta restaurar, via o que houver no dict usado em execução. Caso não consiga, retornará um novo json vazio.
