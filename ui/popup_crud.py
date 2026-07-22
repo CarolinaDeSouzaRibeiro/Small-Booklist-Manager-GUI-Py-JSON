c7ef4a30320d11a64107bbf89308639d99020ae6
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2026-07-22 11:37:35 -0300

    popup crud minor changes

--- ui/popup_crud.py
+++ ui/popup_crud.py
@@ -14,8 +14,6 @@ def subbtn_salvar_livro(root, biblioteca, livro, titulo, autor, categoria, ano, 
             'avaliacao': int(avaliacao) if avaliacao else None,
         }
 
-        # print(f'\nlivro id:{livro_id}\ndict atualizado:{livro_atualizado_dict}\n')
-
         if is_edicao:
             biblioteca.editar_livro(livro.id, livro_atualizado_dict)
         else:
@@ -25,17 +23,19 @@ def subbtn_salvar_livro(root, biblioteca, livro, titulo, autor, categoria, ano, 
         start_ui(root)
 
 def popup_crud_livro(biblioteca, root, colunas_labels_para_telas_crud, livro_obj=None):
-
-    tela_edicao = tk.Toplevel(root)
-    tela_edicao.title("Editar Livro" if livro_obj else "Adicionar Livro")
-    tela_edicao.geometry("300x500")
+    '''Para adição (Create) e edição (Update) de livros pela GUI.
+    Considerado adição se nenhum livro pre-existente for passado em livro_obj
+    '''
+    tela_popup = tk.Toplevel(root)
+    tela_popup.title(f'{ "Editar" if livro_obj else "Adicionar" } Livro')
+    tela_popup.geometry("300x380")
 
     entries = {}
 
     #campos de edição principais
     for label, atrib in colunas_labels_para_telas_crud.items():
-        tk.Label(tela_edicao, text=f"{label}:").pack()
-        entry = tk.Entry(tela_edicao)
+        tk.Label(tela_popup, text=f"{label}:").pack()
+        entry = tk.Entry(tela_popup)
         if livro_obj:
             entry.insert(0, getattr(livro_obj, atrib))
         entries[atrib] = entry
@@ -44,28 +44,25 @@ def popup_crud_livro(biblioteca, root, colunas_labels_para_telas_crud, livro_obj
     #para avaliação, cria um combobox com opções de 1 a 5, e apenas disponivel se lido for verdadeiro
     #mas nao da pack ainda
     avaliacao_var = tk.StringVar(value=getattr(livro_obj, 'avaliacao', ""))
-    avaliacao_combobox = ttk.Combobox(tela_edicao, textvariable=avaliacao_var, values=[i for i in range(1, 6)], state="readonly")
+    avaliacao_combobox = ttk.Combobox(tela_popup, textvariable=avaliacao_var, values=[i for i in range(1, 6)], state="readonly")
     #evento de atualizacao de estado é trigado ao mudar combobox
     avaliacao_combobox.bind("<<ComboboxSelected>>", lambda event: atualizar_estado_combobox(avaliacao_combobox, lido_var, avaliacao_var, btn_salvaradd))
 
     #checkbox de lido
     lido_var = tk.BooleanVar(value=getattr(livro_obj, 'lido', False))
-    tk.Label(tela_edicao, text="Lido:").pack()
-    tk.Checkbutton(tela_edicao, text="Lido", variable=lido_var, command=lambda: atualizar_estado_combobox(avaliacao_combobox, lido_var, avaliacao_var, btn_salvaradd)).pack()
+    tk.Label(tela_popup, text="Lido:").pack()
+    tk.Checkbutton(tela_popup, text="Lido", variable=lido_var, command=lambda: atualizar_estado_combobox(avaliacao_combobox, lido_var, avaliacao_var, btn_salvaradd)).pack()
 
     #label da combobox de avaliação
-    tk.Label(tela_edicao, text="Avaliação:").pack()
+    tk.Label(tela_popup, text="Avaliação:").pack()
 
     #ao habilitar/desabilitar o checkbutton, atualiza estado da combobox
     avaliacao_combobox.pack()
 
     #Botão de salvar/adicionar.
     #Desabilitado se os campos obrigatorios forem vazios ou se lido for verdadeiro enquanto avaliação for vazia
-    
-
-
     btn_salvaradd = tk.Button(
-        tela_edicao,
+        tela_popup,
         text="Salvar" if livro_obj else "Adicionar",
         command=lambda: subbtn_salvar_livro(
                 root,
@@ -77,11 +74,10 @@ def popup_crud_livro(biblioteca, root, colunas_labels_para_telas_crud, livro_obj
                 entries['ano'].get(),
                 lido_var.get(),
                 avaliacao_var.get(),
-                tela_edicao,
+                tela_popup,
                 is_edicao=bool(livro_obj) # True se for edição, False se for adição de novo livro
             )
         )
     btn_salvaradd.pack(pady=10)
 
     atualizar_estado_combobox(avaliacao_combobox, lido_var, avaliacao_var, btn_salvaradd) #atualizacao inicial
-
